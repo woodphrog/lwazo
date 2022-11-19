ad254f092fd4ef70a514c72da7b884e729e192dc
Ezra Marin <marinezra02@gmail.com>
2022-11-19 15:50:30 -0500

    title

--- app/src/main/java/dev/hongjun/lwazo/ConversationListActivity.kt
+++ app/src/main/java/dev/hongjun/lwazo/ConversationListActivity.kt
@@ -1,15 +1,23 @@
 package dev.hongjun.lwazo
 
+import android.graphics.fonts.FontStyle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.TopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import dev.hongjun.lwazo.ui.theme.LoiseauBleuTheme
 
 class ConversationListActivity : ComponentActivity() {
@@ -22,7 +30,8 @@ class ConversationListActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    //Greeting("Android")
+                    messagesListTopBar(title = "Messages")
                 }
             }
         }
@@ -36,7 +45,26 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
+@Composable
+fun messagesListTopBar(title : String){
+    Row(){
+    TopAppBar(
+        title = {
+            Text(
+                text = title,
+                style = TextStyle(
+                    textAlign = TextAlign.Center,
+                    fontStyle = androidx.compose.ui.text.font.FontStyle(1),
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            )
+        },
 
+        backgroundColor = Color.White
+    )
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
